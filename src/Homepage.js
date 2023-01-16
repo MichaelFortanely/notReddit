d8f9985af6b92c251d43ec05e638ae17e5d29bc2
Michael Fortanely <michaelj.fortanely@gmail.com>
2023-01-16 13:07:24 -0600

    able to retrieve search suggestions from backend

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,20 +1,56 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Logo from './images/reddit.jpg'
-import Post from './Post'
+import { useState } from 'react'
+import { useEffect } from 'react'
 // href="subs/
 
 const Homepage = () => {
 let usersFavSubreddit = ['AITA', 'Books', 'Pics', 'Surreal Memes']
+const [search, setSearch] = useState('')
+useEffect(() => {
+    //do a fetch for all of the posts in this subreddit
+    async function getapi(url) {
 
-   
+        // Storing response
+        const response = await fetch(url, {
+            mode: "cors",
+            headers: {"Content-Type": "application/json"}});
+        
+        // Storing data in form of JSON
+        var data = await response.json();
+        console.log(data);
+        // setPostsStub(data)
+    }
+    getapi(`http://localhost:9000/posts/search/${search}`)
+    console.log('search: ' + search)
+}, [search])
   return (
     <div id="#scroll-parent">
         <div>
             <nav className='top'>
                 <h3>notReddit</h3>
                 <img style={{width: '60px', left: '160px', top: '10px'}} src={Logo} alt="Upside down reddit logo"/>
-                <input type="text" placeholder="Search subreddit by name.."></input>
+                {/* <input type="text"  value={search}
+            onChange={(e) => setSearch(e.target.value)} onSubmit={() => console.log('Search is' + search)}></input> */}
+
+<form className='searchForm' onSubmit={(e) => { e.preventDefault() 
+        console.log('search is: ' + search)
+        let suggestions = ""
+        console.log('suggestions: ' + suggestions)
+        }}>
+        <label htmlFor='search'></label>
+        <input
+            id='search'
+            type='text'
+            role='searchbox'
+            placeholder="Search posts by title or content..."
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+        />
+    </form>
+
+
                 <button className='login-button' onClick={() => console.log('login')}>Log in</button>
             </nav>
             <div style={{position: 'fixed', top: '100px'}}>
