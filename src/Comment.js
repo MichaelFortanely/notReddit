9c0d1d16d57daff7406d3e6fc46018b439b997e0
Michael Fortanely <michaelj.fortanely@gmail.com>
2023-01-19 00:01:28 -0600

    started to work on comment formatting

--- src/Comment.js
+++ src/Comment.js
@@ -15,14 +15,24 @@ const Comment = ({commentID}) => {
          var data = await response.json();
          console.log('Comment\n\n\nhskhskskhksh: ')
          console.log(data[0].body)
+         console.log(typeof comment.timestamp)
          setComment(data[0])
-
          }
      _internal(`${BACKEND_URL}comments/by_id/${commentID}`)
  }, [])
   return (
     <>
-    <div>{comment.body}{comment.user}{comment.upvotes}{comment.timestamp}</div>
+    <div style={{backgroundColor: '#f5eeed', padding: "15px", margin: '5px', width: '70%'}} >
+      <div style={{position: 'relative', left: '20vw'}}>
+        Posted by u/{comment.user} on {new Date(comment.timestamp).toLocaleString("en-us", { month: "long", day: "numeric", year: "numeric" })}
+      </div><br/>
+      <div style={{left: '5vw', position: 'relative'}}>
+        {comment.body}
+      </div>
+      <div><br/>
+        {comment.upvotes}
+      </div>
+    </div>
     </>
   )
 }
