6c3f203319a8caca4eaca02ffe6d5bb3058b1ce9
Michael Fortanely <michaelj.fortanely@gmail.com>
2023-01-23 14:12:11 -0600

    change frontend URL

--- src/config.js
+++ src/config.js
@@ -2,4 +2,4 @@ export const BACKEND_URL = 'https://nodejs-reddit-backend-rest.herokuapp.com/';
 
 export const usersFavSubreddit = ['AITA', 'books', 'unpopularopinion', 'Trueoffmychest', 'explainlikeimfive', 'personalfinance'];
 
-export const FRONTEND_URL = 'http://localhost:3000/';
+export const FRONTEND_URL = 'https://react-frontend.herokuapp.com/';
